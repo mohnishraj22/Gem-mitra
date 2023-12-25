5029d577d51541fa93866455007faee0146382f7
mohnishraj22 <94984213+mohnishraj22@users.noreply.github.com>
2023-12-25 14:21:05 +0530

    Main commit

--- Gem_mitra/settings.py
+++ Gem_mitra/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zkef&m@l9@4ocgi_do&0=rl)uv6w75wn5oxw*w9re2qk246itl"
-
+#SECRET_KEY = "django-insecure-zkef&m@l9@4ocgi_do&0=rl)uv6w75wn5oxw*w9re2qk246itl"
+SECRET_KEY =os.environ.get("SECRET_KEY","secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get("DEBUG",)
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -112,7 +112,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+STATIC_ROOT="staticfiles"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -121,6 +121,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
